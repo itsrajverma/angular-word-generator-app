0c509f13687c531e24dab67a88dbbc0124928269
Raj Verma <info.mrverma@gmail.com>
2020-06-11 07:30:59 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+import arrayWords from  "../utils/words";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wordgen-app';
+  title = 'Word generator App';
+
+  count = 10;
+  words = '';
+
+  handleSlideChange(newLimit){
+    this.count = newLimit;
+  }
+
+  generate(){
+    this.words = arrayWords.slice(0,this.count).join(" ");
+  }
+
 }
